ff9a6ced380c78900bb8cc823cab63ce4fa240fd
Thopia-afk <133740753+Thopia-afk@users.noreply.github.com>
2023-09-06 00:47:49 +0200

    sumtang

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -3,13 +3,20 @@
 from person import Person
 
 def main():
-	f = Person(50)
-	print(f.getAge())
-	print(f.getDecades())
+	f = Person(5)
+	print(f.get())
+	f.set(8)
+	print(f.get())
 
-	f.setAge(51)
-	print(f.getAge())
-	print(f.getDecades())
+# def main():
+# 	f = Person(50)
+# 	print(f.getAge())
+# 	print(f.getDecades())
+
+# 	f.setAge(51)
+# 	print(f.getAge())
+# 	print(f.getDecades())
+
 
 if __name__ == '__main__':
 	main()
