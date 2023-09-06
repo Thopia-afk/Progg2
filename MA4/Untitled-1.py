f6d613d811cd07962ad599770d8755104e2d783f
Thopia-afk <133740753+Thopia-afk@users.noreply.github.com>
2023-09-06 11:33:46 +0200

    now

--- MA4/Untitled-1.py
+++ MA4/Untitled-1.py
@@ -2,11 +2,28 @@
 # n = [i for i in range(30, 45, 1)]
 # print(n)
 
-def fib(n):
-	if n <= 1:
-		return n
-	else:
-		return(fib(n-1) + fib(n-2))
+# def fib(n):
+# 	if n <= 1:
+# 		return n
+# 	else:
+# 		return(fib(n-1) + fib(n-2))
 
-print(1+1)
-print(fib(40))
+# print(1+1)
+# print(fib(40))
+
+# import matplotlib.pyplot as plt
+ 
+# # Creating data
+# x = ['2010', '2002', '2004', '2006', '2008']
+# y = [10, 25, 50, 15, 8]
+ 
+# # Plotting barchart
+# plt.bar(x, y)
+ 
+# # Saving the figure.
+# plt.savefig("output-1.jpg")
+ 
+# # Saving figure by changing parameter values
+# plt.savefig("output-2", facecolor='g', bbox_inches="tight",
+#             pad_inches=0.3, transparent=True)
+# plt.show()
