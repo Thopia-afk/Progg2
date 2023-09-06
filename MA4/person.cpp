d774d48cb645c2f7134f761f23a3af82ba150e67
Thopia-afk <133740753+Thopia-afk@users.noreply.github.com>
2023-09-06 09:14:11 +0200

    f

--- MA4/person.cpp
+++ MA4/person.cpp
@@ -32,7 +32,7 @@ extern "C"{
 	Person* Person_new(int a) {return new Person(a);}
 	int Person_getAge(Person* person) {return person->getAge();}
 	void Person_setAge(Person* person, int a) {person->setAge(a);}
-	double Person_getDecades(Person* person) {return person->getDecades();}
+	int Person_getDecades(Person* person) {return person->getDecades();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
